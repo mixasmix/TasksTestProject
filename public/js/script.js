e26a3ea88185588a5bc1b8c10f188836a8a0bc22
m.ivanov <m.ivanov@fingineers.ru>
2022-07-07 16:21:04 +0300

    еще чуть верстки и яваскрипта

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,25 @@
 $('#submitUserData').on('click', function () {
   let userData = {
-    name: $('#userName').val(),
+    "name": $('#userName').val(),
   };
 
   $.ajax({
     url: '/user',
     method: "POST",
-    data: userData
+    data: JSON.stringify(userData)
   })
-    .done(function () {
+    .done(function (serverData) {
+      let data = $.parseJSON(serverData);
+
       $('#register').addClass('visually-hidden');
 
       let userContainer = $('#registeredUser');
 
+      userContainer.find('#userNameParagraph').text(data.name);
+      userContainer.find('#userRegisterDate').text(data.created_at);
+      userContainer.find('#userId').text(data.id);
+
+      userContainer.removeClass('visually-hidden');
     });
   console.log(userData);
 });
